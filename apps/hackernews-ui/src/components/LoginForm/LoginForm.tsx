c0303282c5cec7716e1a3e2611ddc98220fc5bc0
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2021-12-23 22:59:17 +0200

    feat(hackernews-ui): move next.js config to .mjs

--- apps/hackernews-ui/src/components/LoginForm/LoginForm.tsx
+++ apps/hackernews-ui/src/components/LoginForm/LoginForm.tsx
@@ -19,18 +19,18 @@ import { createResolver, FormShape } from './resolver';
 export const LoginForm: React.FC = () => {
     const router = useRouter();
     const isAuthenticated = vars.useIsAuthenticated();
-    const [ isLogin, setIsLogin ] = useState(true);
-    const [ isFetching, setIsFetching ] = useState(false);
+    const [isLogin, setIsLogin] = useState(true);
+    const [isFetching, setIsFetching] = useState(false);
 
     const createToast = useToasts();
 
     const form = useForm<FormShape>({
-        resolver:      createResolver(isLogin),
-        mode:          'all',
+        resolver: createResolver(isLogin),
+        mode: 'all',
         defaultValues: {
-            name:            __DEV__ ? 'Jack' : '',
-            email:           __DEV__ ? 'test@email.io' : '',
-            password:        __DEV__ ? '12345' : '',
+            name: __DEV__ ? 'Jack' : '',
+            email: __DEV__ ? 'test@email.io' : '',
+            password: __DEV__ ? '12345' : '',
             confirmPassword: __DEV__ ? '12345' : '',
         },
     });
@@ -43,7 +43,7 @@ export const LoginForm: React.FC = () => {
         vars.isAuthenticated(true);
     };
 
-    const [ loginMutation ] = gql.useLoginMutation({
+    const [loginMutation] = gql.useLoginMutation({
         variables: form.getValues(),
         onCompleted(data) {
             saveToken(data.login);
@@ -55,7 +55,7 @@ export const LoginForm: React.FC = () => {
             createToast({ type: 'error', text: error.message, delay: 10000 });
         },
     });
-    const [ signupMutation ] = gql.useSignupMutation({
+    const [signupMutation] = gql.useSignupMutation({
         variables: form.getValues(),
         onCompleted(data) {
             saveToken(data.signup);
@@ -63,15 +63,10 @@ export const LoginForm: React.FC = () => {
         onError(error) {
             let fieldName = null;
 
-            const fields: Array<keyof FormShape> = [
-                'name',
-                'email',
-                'password',
-            ];
+            const fields: Array<keyof FormShape> = ['name', 'email', 'password'];
 
-            fields.forEach(field => {
-                error.message.toLowerCase().includes(field)
-                    && (fieldName = field);
+            fields.forEach((field) => {
+                error.message.toLowerCase().includes(field) && (fieldName = field);
             });
 
             createToast({ type: 'error', text: error.message, delay: 10000 });
@@ -108,69 +103,63 @@ export const LoginForm: React.FC = () => {
     }
 
     return (
-        <form onSubmit = { form.handleSubmit(submit) }>
+        <form onSubmit={form.handleSubmit(submit)}>
             <GUI.Spacer />
             <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-            <GUI.Spacer h = { 2 } />
+            <GUI.Spacer h={2} />
 
-            <Fieldset css = 'max-width: 300px;' disabled = { isFetching }>
+            <Fieldset css='max-width: 300px;' disabled={isFetching}>
                 {!isLogin && (
                     <Input
                         autoFocus
-                        formState = { form.formState }
-                        placeholder = 'Your name'
-                        register = { form.register('name') }
+                        formState={form.formState}
+                        placeholder='Your name'
+                        register={form.register('name')}
                     />
                 )}
 
                 <Input
                     autoFocus
-                    formState = { form.formState }
-                    placeholder = 'Your email address'
-                    register = { form.register('email') }
+                    formState={form.formState}
+                    placeholder='Your email address'
+                    register={form.register('email')}
                 />
                 <Input
-                    formState = { form.formState }
-                    placeholder = 'Choose a safe password'
-                    register = { form.register('password') }
-                    type = 'password'
+                    formState={form.formState}
+                    placeholder='Choose a safe password'
+                    register={form.register('password')}
+                    type='password'
                 />
                 {!isLogin && (
                     <Input
-                        formState = { form.formState }
-                        placeholder = 'Confirm password'
-                        register = { form.register('confirmPassword') }
-                        type = 'password'
+                        formState={form.formState}
+                        placeholder='Confirm password'
+                        register={form.register('confirmPassword')}
+                        type='password'
                     />
                 )}
 
-                <S.Controls>
-                    <GUI.Button
-                        auto
-                        disabled = { isFetching }
-                        htmlType = 'submit'
-                        loading = { isFetching }
-                    >
+                <Styled.Controls>
+                    <GUI.Button auto disabled={isFetching} htmlType='submit' loading={isFetching}>
                         {isLogin ? 'login' : 'create account'}
                     </GUI.Button>
 
                     <GUI.Button
                         auto
                         ghost
-                        disabled = { isFetching }
-                        type = 'secondary'
-                        onClick = { () => setIsLogin(!isLogin) }
-                    >
+                        disabled={isFetching}
+                        type='secondary'
+                        onClick={() => setIsLogin(!isLogin)}>
                         {isLogin ? 'create account' : 'back to login'}
                     </GUI.Button>
-                </S.Controls>
+                </Styled.Controls>
             </Fieldset>
         </form>
     );
 };
 
 /* Styles */
-const S = {
+const Styled = {
     Controls: styled.div`
         display: flex;
         gap: 5px;
