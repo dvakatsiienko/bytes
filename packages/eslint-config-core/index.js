8946ac2a5eb6e23ac7b96a7cb49704b31504c2a8
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2021-12-28 23:36:22 +0200

    refactor(packages/eslint-config-core): colocate core files in lib directory

--- packages/eslint-config-core/index.js
+++ packages/eslint-config-core/index.js
@@ -3,4 +3,4 @@
 /**
  * @type {import('eslint').Linter.Config<import('eslint').Linter.RulesRecord)>}
  */
-module.exports = { extends: [ './javascript', './typescript', './react' ] };
+module.exports = { extends: ['./lib/javascript', './lib/typescript', './lib/react'] };
--- packages/eslint-config-core/lib/rules/complex-ts.js
+++ packages/eslint-config-core/lib/rules/complex-ts.js
@@ -5,12 +5,12 @@ exports.banTsComment = [
     2,
     {
         'ts-expect-error': 'allow-with-description',
-        'ts-ignore':       'allow-with-description',
-        'ts-nocheck':      'allow-with-description',
-        'ts-check':        'allow-with-description',
+        'ts-ignore': 'allow-with-description',
+        'ts-nocheck': 'allow-with-description',
+        'ts-check': 'allow-with-description',
     },
 ];
 
-const [ , commaDangleRuleOpts ] = style.rules[ 'comma-dangle' ];
+const [, commaDangleRuleOpts] = style.rules['comma-dangle'];
 
 exports.commaDangleRuleOpts = commaDangleRuleOpts;
