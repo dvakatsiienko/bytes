e9c9c156a3c8dbf474a4a4051d1670136b425c32
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-04-29 17:15:12 +0300

    🔧 chore(eslint): update eslint configuration The eslint configuration has been updated to include the following changes: - The parserOptions object has been added to specify the sourceType and ecmaVersion. - The .eslintrc.js files in the apps/hackernews, apps/simple-docs, and apps/simple-web directories have been deleted. - The .eslintrc.mjs file in the apps/space-explorer-api directory has been deleted. - The .eslintrc.js file in the apps/space-explorer-ui directory has been deleted. - The rules object in the core configuration has been updated to include the indent rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
+/** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
     root:    true,
     extends: 'core',
+    globals: { $: true },
+    rules:   {
+        // TODO: move to package config
+        '@typescript-eslint/no-empty-interface': 0,
+
+        'react/require-default-props':                     0, // defaultProps got deprecated, review this rule
+        'jsx-a11y/no-noninteractive-element-interactions': 0,
+
+        'import/no-cycle': 0,
+
+        'arrow-parens':         [ 1, 'always' ],
+        'key-spacing':          [ 1, { beforeColon: false, afterColon: true, align: 'value' }],
+        'object-curly-newline': [
+            1,
+            {
+                ObjectExpression:  { multiline: true },
+                ObjectPattern:     { multiline: true },
+                ImportDeclaration: { multiline: true },
+                ExportDeclaration: { multiline: true },
+            },
+        ],
+        'prefer-destructuring': 0,
+        camelcase:              [ 1, { allow: [ '^unstable_', 'grant_type', 'access_token', 'refresh_token' ] }],
+    },
 };
--- apps/space-explorer-ui/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('eslint').ESLint.ConfigData} */
-module.exports = {
-    extends: 'dva',
-    rules:   {
-        'arrow-parens': [ 2, 'always' ],
-    },
-};
