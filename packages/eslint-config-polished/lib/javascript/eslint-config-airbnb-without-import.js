8926595e588874cc1ef53f12a3ca94ef932c9c36
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-04-30 20:47:32 +0300

    🔥 chore(eslint-config-polished): remove eslint-config-airbnb-base package and related configuration file 📦 chore(eslint-config-polished): update package version to 0.0.1 The eslint-config-airbnb-base package and its related configuration file have been removed as they are no longer needed. The package.json file has been updated to reflect the removal of the package and the version has been updated to 0.0.1.

--- packages/eslint-config-polished/lib/javascript/eslint-config-airbnb-without-import.js
+++
@@ -1,9 +0,0 @@
-/* Core */
-const airbnbBase = require('eslint-config-airbnb-base');
-
-const airbnbConfigWithoutImport = {
-    ...airbnbBase,
-    extends: airbnbBase.extends.filter((path) => !path.includes('import')),
-};
-
-module.exports = airbnbConfigWithoutImport;
