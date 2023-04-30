fde72f80e9ef14bdfb88b4c55e9607d828fffd73
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-01 01:31:01 +0300

    🔥 chore(eslint): remove unused rules from javascript.js configuration file The indent and quotes rules were removed from the javascript.js configuration file as they were not being used.

--- packages/eslint-config-polished/javascript.js
+++ packages/eslint-config-polished/javascript.js
@@ -1,10 +1,4 @@
 /* eslint-env: Node */
 
 /** @type {import('eslint').ESLint.ConfigData} */
-module.exports = {
-    extends: [ './lib/javascript' ],
-    rules:   {
-        indent: [ 1, 4 ],
-        quotes: [ 2, 'single' ],
-    },
-};
+module.exports = { extends: [ './lib/javascript' ]};
