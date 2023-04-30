60f597534443f201de36194335b3162a5909d3e0
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-04-30 20:40:56 +0300

    🔥 refactor(complex-js.js): remove noRestrictedSyntax rule The noRestrictedSyntax rule has been removed from the eslint-config-polished package. This rule was overriding the airbnb-config-base and was dropping the "for of" restriction. The rule was removed to improve the consistency of the package with the airbnb-config-base.

--- packages/eslint-config-polished/lib/rules/complex-js.js
+++ packages/eslint-config-polished/lib/rules/complex-js.js
@@ -1,29 +1,9 @@
-exports.noRestrictedSyntax = [
-    // ? airbnb-config-base override — drop «for of» restriction
-    2,
-    {
-        selector: 'ForInStatement',
-        message:
-            'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
-    },
-    {
-        selector: 'LabeledStatement',
-        message:
-            'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
-    },
-    {
-        selector: 'WithStatement',
-        message:
-            '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
-    },
-];
-
 exports.noUnderscoreDangle = [ 2, { allow: [ '__typename', '__REDUX_DEVTOOLS_EXTENSION_COMPOSE__' ] }];
 
 exports.noUnusedExpressions = [
-    // ? airbnb-config-base override — allow all to loose the rule strictness
-    2,
+    1,
     {
+        // ? allow all to loose the rule strictness
         allowShortCircuit:    true,
         allowTernary:         true,
         allowTaggedTemplates: true,
