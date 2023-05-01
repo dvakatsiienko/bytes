8f9b7ccfcfac2d011a14864b5cf614e150e0baad
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-01 18:03:59 +0300

    🔧 chore(eslint): update eslint configuration ✨ feat(eslint): add support for typescript strict type checking and stylistic type checking 🔧 chore(eslint): update eslint configuration to disable indent rule for TypeScript files 🔧 chore(eslint): update eslint configuration to include only root level TypeScript files

--- packages/eslint-config-polished/lib/typescript.js
+++ packages/eslint-config-polished/lib/typescript.js
@@ -8,54 +8,68 @@ const complex = require('./rules/complex-ts');
 module.exports = {
     overrides: [
         {
-            files:   [ '*.ts', '*.tsx', '*.mts', '*.mtsx' ],
-            extends: [ 'plugin:@typescript-eslint/recommended' ],
-            plugins: [ '@typescript-eslint' ],
-            parser:  '@typescript-eslint/parser',
-            rules:   {
+            files:   [ '*.ts', '*.tsx', '*.mts', '*.mtsx', '*.cts', '*.ctsx' ],
+            extends: [
+                // 'plugin:@typescript-eslint/recommended',
+                // 'plugin:@typescript-eslint/recommended-type-checked',
+                'plugin:@typescript-eslint/strict-type-checked',
+                'plugin:@typescript-eslint/stylistic-type-checked',
+            ],
+            plugins:       [ '@typescript-eslint' ],
+            parser:        '@typescript-eslint/parser',
+            parserOptions: {
+                ecmaVersion:             'latest',
+                tsconfigRootDir:         __dirname,
+                project:                 true,
+                projectFolderIgnoreList: [
+                    '**/node_modules/**',
+                    '**/.next/**',
+                    '**/dist/**',
+                    '**/build/**',
+                    '**/out/**',
+                ],
+            },
+            rules: {
+                'no-undef': 0, // ? ts(2304), extended from eslint-recommended → all shareable configs
+                /**
+                 * ? Recommended to disable this to save on performance. https://v6--typescript-eslint.netlify.app/linting/troubleshooting/performance-troubleshooting#the-indent--typescript-eslintindent-rules
+                 * ? Not included in shareable config.
+                 */
+                indent:     0,
+
                 // TODO
                 // '@typescript-eslint/no-empty-interface': 0,
                 // '@typescript-eslint/comma-dangle':       [ 1, 'always-multiline' ],
                 // '@typescript-eslint/no-shadow':          0,
                 // TODO configure this rule to allow missed parameters ... or? not.
                 // '@typescript-eslint/no-unused-vars':     0,
-
                 // ? Rules
-                '@typescript-eslint/explicit-module-boundary-types': 0,
-                '@typescript-eslint/ban-ts-comment':                 complex.banTsComment,
-                '@typescript-eslint/consistent-type-assertions':     [ 2, { assertionStyle: 'as' }],
-                '@typescript-eslint/member-delimiter-style':         2,
-                '@typescript-eslint/method-signature-style':         2,
-                '@typescript-eslint/no-require-imports':             2,
-                '@typescript-eslint/prefer-optional-chain':          2,
-
+                // '@typescript-eslint/explicit-module-boundary-types': 0,
+                // '@typescript-eslint/ban-ts-comment':                 complex.banTsComment,
+                // '@typescript-eslint/consistent-type-assertions':     [ 2, { assertionStyle: 'as' }],
+                // '@typescript-eslint/member-delimiter-style':         2,
+                // '@typescript-eslint/method-signature-style':         2,
+                // '@typescript-eslint/no-require-imports':             2,
+                // '@typescript-eslint/prefer-optional-chain':          2,
                 // ? Extension Rules
-                'brace-style':                    0,
-                '@typescript-eslint/brace-style': 2,
-
-                'comma-dangle': 0,
+                // 'brace-style':                    0,
+                // '@typescript-eslint/brace-style': 2,
+                // 'comma-dangle': 0,
                 // '@typescript-eslint/comma-dangle': [ 2, complex.commaDangleRuleOpts ],
-
-                'comma-spacing':                    0,
-                '@typescript-eslint/comma-spacing': 2,
-
-                'default-param-last':                    0,
-                '@typescript-eslint/default-param-last': 2,
-
-                'init-declarations':                    0,
-                '@typescript-eslint/init-declarations': [ 2, 'always' ],
-
-                'no-dupe-class-members':                    0,
-                '@typescript-eslint/no-dupe-class-members': 2,
-
-                'no-duplicate-imports':                    0,
-                '@typescript-eslint/no-duplicate-imports': [ 2, { includeExports: true }],
-
-                'no-shadow':                    0,
-                '@typescript-eslint/no-shadow': 2,
-
-                quotes:                      0,
-                '@typescript-eslint/quotes': [ 2, 'single' ],
+                // 'comma-spacing':                    0,
+                // '@typescript-eslint/comma-spacing': 2,
+                // 'default-param-last':                    0,
+                // '@typescript-eslint/default-param-last': 2,
+                // 'init-declarations':                    0,
+                // '@typescript-eslint/init-declarations': [ 2, 'always' ],
+                // 'no-dupe-class-members':                    0,
+                // '@typescript-eslint/no-dupe-class-members': 2,
+                // 'no-duplicate-imports':                    0,
+                // '@typescript-eslint/no-duplicate-imports': [ 2, { includeExports: true }],
+                // 'no-shadow':                    0,
+                // '@typescript-eslint/no-shadow': 2,
+                // quotes:                      0,
+                // '@typescript-eslint/quotes': [ 2, 'single' ],
             },
         },
     ],
