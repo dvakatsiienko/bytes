b20f7217dcf9d2937562f0d1e78e71c0747a55ad
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-04 20:00:52 +0300

    🔥 chore(.eslintrc.js): remove commented code and simplify configuration The commented code was not being used and was just cluttering the file. The configuration was simplified by removing the overrides array and keeping only the root and extends properties.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,6 @@
  * @type {import('eslint').Linter.Config}
  */
 module.exports = {
-    root:      true,
-    extends:   'polished/typescript',
-    // overrides: [
-    //     // ? pacakges
-    //     {
-    //         files:   [ './packages/eslint-config-polished/**/*.js' ],
-    //         extends: 'polished/javascript',
-    //     },
-    // ],
+    root:    true,
+    extends: 'polished/typescript',
 };
