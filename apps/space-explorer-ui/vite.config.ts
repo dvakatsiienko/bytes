149c99ac6d4f5c62c26e0333f7c6ba3dc320a446
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-07 01:24:12 +0300

    🐛 fix(vite.config.ts): remove unnecessary line breaks in the export statement The export statement was unnecessarily split into multiple lines, which can be simplified to a single line for better readability.

--- apps/space-explorer-ui/vite.config.ts
+++ apps/space-explorer-ui/vite.config.ts
@@ -4,6 +4,4 @@ import vitePluginReactSwc from '@vitejs/plugin-react-swc';
 import vitePluginProgress from 'vite-plugin-progress';
 import vitePluginTsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-    plugins: [ vitePluginReactSwc(), vitePluginProgress(), vitePluginTsconfigPaths() ],
-});
+export default defineConfig({ plugins: [ vitePluginReactSwc(), vitePluginProgress(), vitePluginTsconfigPaths() ]});
