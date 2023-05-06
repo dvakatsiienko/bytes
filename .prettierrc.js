d54bb80f69496fd1ee0c86d2bbd0e8261a8614f1
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-07 01:07:47 +0300

    🎉 feat(.prettierrc.js): add Prettier configuration file This commit adds a Prettier configuration file to the project root directory. The configuration file sets the print width to 100, tab width to 4, uses spaces instead of tabs, enforces semicolons, single quotes, and trailing commas for all objects. It also sets jsxSingleQuote to true and bracketSpacing to true. This configuration file will help maintain consistent code formatting across the project.

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+    printWidth:     100,
+    tabWidth:       4,
+    useTabs:        false,
+    semi:           true,
+    singleQuote:    true,
+    quoteProps:     'as-needed',
+    jsxSingleQuote: true,
+    trailingComma:  'all',
+    bracketSpacing: true,
+};
