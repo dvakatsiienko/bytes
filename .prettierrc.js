a62a6f570c0ed90549821820c6e61dc0395eb09b
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-08 00:34:11 +0300

    🚀 chore(prettier): add .prettierrc.js to each package to improve code formatting consistency Each package now has its own .prettierrc.js file, which allows for more granular control over code formatting. This change was made to improve code formatting consistency across the entire monorepo.

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,3 +2,5 @@
  * @type {import('prettier').Config}
  */
 module.exports = { ...require('prettier-config-polished') };
+
+// ? 08.05.2023: coudn't make single .prettierrc to cover entire monorepo, so each package has its own .prettierrc
