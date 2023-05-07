2b61ed8ba9a4fb57646c10d1fc3f775fa14a0ab0
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-07 21:29:47 +0300

    🎉 feat(prettier-config-polished): add Prettier configuration package A new Prettier configuration package has been added to the project. The package includes a `.prettierrc.js` file with the Prettier configuration, an `.eslintignore` file to ignore files that should not be linted by ESLint, and an `index.js` file that exports the configuration. The package is published to npm and can be used by other projects to maintain consistent code formatting.

---
+++ packages/prettier-config-polished/.prettierrc.js
@@ -0,0 +1,18 @@
+/**
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+    tabWidth:        4,
+    printWidth:      100,
+    useTabs:         false,
+    semi:            true,
+    singleQuote:     true,
+    quoteProps:      'as-needed',
+    jsxSingleQuote:  true,
+    trailingComma:   'all',
+    bracketSpacing:  true,
+    bracketSameLine: true,
+    arrowParens:     'always',
+    proseWrap:       'always',
+    endOfLine:       'lf',
+};
