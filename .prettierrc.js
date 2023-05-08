8217e72b6c40571a4b989db54926ccd24e00f68e
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-08 14:26:06 +0300

    🔥 refactor(.prettierrc.js): remove duplicate comment and add explanation for the change The duplicate comment was removed to improve code readability. A new comment was added to explain why each package has its own .prettierrc file.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
+// ? 08.05.2023: coudn't make single .prettierrc to cover entire monorepo, so each package has its own .prettierrc
+
 /**
  * @type {import('prettier').Config}
  */
 module.exports = { ...require('prettier-config-polished') };
-
-// ? 08.05.2023: coudn't make single .prettierrc to cover entire monorepo, so each package has its own .prettierrc
