8e8652ae114dec343aa426310cff4513c9ce2b69
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-21 17:45:27 +0300

    🎨 style(layout.tsx): add global styles to the layout component 🎨 style(page.tsx): add styles to the profile page component 🎨 style(global.scss): add global styles for body and main tags 🎨 style(index.scss): import theme.scss to index.scss to use it in the app

--- apps/profile/src/app/page.tsx
+++ apps/profile/src/app/page.tsx
@@ -1,3 +1,56 @@
+/* Instruments */
+import styles from './index-page.module.scss';
+
 export default () => {
-    return <h1>Index page</h1>;
+    return (
+        <main className = { styles.profile }>
+            <h1>Profile</h1>
+            <section>
+                <div>
+                    <span className = 'key'>name</span>
+                    <span className = 'value'>Dima Vakatsiienko</span>
+                </div>
+
+                <div>
+                    <span className = 'key'>email</span>
+                    <span className = 'value'>imagnum.satellite@gmail.com</span>
+                </div>
+
+                <div>
+                    <span className = 'key'>location</span>
+                    <span className = 'value'>Ukraine, Kyiv</span>
+                </div>
+
+                <div>
+                    <span className = 'key'>links</span>
+                    <span className = 'value'>
+                        <a
+                            href = 'https://telegra.ph/SeniorLead-Frontend-Developer-10-02'
+                            rel = 'noreferrer noopener'
+                            target = '_blank'>
+                            Cover Letter
+                        </a>
+                        <a
+                            href = 'https://github.com/dvakatsiienko'
+                            rel = 'noreferrer noopener'
+                            target = '_blank'>
+                            Github
+                        </a>
+                        <a
+                            href = 'https://www.linkedin.com/in/dima-vakatsiienko-a20271100/'
+                            rel = 'noreferrer noopener'
+                            target = '_blank'>
+                            Linkedin
+                        </a>
+                        <a
+                            href = 'https://t.me/shining1488'
+                            rel = 'noreferrer noopener'
+                            target = '_blank'>
+                            Telegram
+                        </a>
+                    </span>
+                </div>
+            </section>
+        </main>
+    );
 };
