89e84e213c02b8ede473bc302b3ea2b5b7ec4b07
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-26 19:57:45 +0300

    🚀 chore(profile): add request-ip package to dependencies 🐛 fix(getVisits.ts): add next property to fetch options to enable revalidation 🐛 fix(route.ts): add cip property to GetVisitsResponse interface and return it in GET handler ✨ feat(Stats.tsx): add cip property to Stats component output

--- apps/profile/src/app/components/Stats.tsx
+++ apps/profile/src/app/components/Stats.tsx
@@ -2,18 +2,14 @@
 import { getVisits } from '@/api';
 
 export const Stats = async () => {
-    // const visits = await getVisits();
-    const visitsJSON = await fetch(`${ process.env.NEXT_PUBLIC_API_URL }/api/visits`, {
-        method: 'GET',
-        // cache:  'no-store',
-        next:   { revalidate: 1 },
-    }).then((res) => res.text());
-    const visits = JSON.parse(visitsJSON);
+    const visits = await getVisits();
 
     return (
         <>
-            <span>Visits ALL: {visits?.visitsAll}</span>{' '}
-            <span>Visits UNIQUE: {visits?.visitsUnique}</span> <span>IP: {visits?.ip}</span>
+            <span>Visits ALL: {visits?.visitsAll}</span>
+            <span>Visits UNIQUE: {visits?.visitsUnique}</span>
+            <span>IP: {visits?.ip}</span>
+            <span>CIP: {visits?.cip}</span>
         </>
     );
 };
