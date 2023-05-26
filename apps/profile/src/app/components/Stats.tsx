38df17764453933736d8581a1ad40f9c88b33c38
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-26 20:22:16 +0300

    🔥 refactor(profile): properly determine and store uniqe visits

--- apps/profile/src/app/components/Stats.tsx
+++ apps/profile/src/app/components/Stats.tsx
@@ -9,7 +9,6 @@ export const Stats = async () => {
             <span>Visits ALL: {visits?.visitsAll}</span>
             <span>Visits UNIQUE: {visits?.visitsUnique}</span>
             <span>IP: {visits?.ip}</span>
-            <span>CIP: {visits?.cip}</span>
         </>
     );
 };
