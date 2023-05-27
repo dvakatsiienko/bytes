e2ad88a6e9abe753bc3176be1c6a7b923ecd1d0c
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-27 14:38:29 +0300

    🐛 fix(profile): transform headers into a plain object to fix request-ip usage in next.js ✨ feat(profile): add conditional to only update kv store in production environment to avoid development data pollution 🔥 refactor(profile): rename UNIQUE_IP_KEY to UNIQUE_IP_SET_KEY to improve semantics

--- apps/profile/src/app/api/visits/route.ts
+++ apps/profile/src/app/api/visits/route.ts
@@ -1,31 +1,35 @@
 /* Core */
 import { kv } from '@vercel/kv';
 import { NextResponse, type NextRequest } from 'next/server';
+import requestIp from 'request-ip';
 
 /* Instruments */
 import type { GetVisitsResponse } from '@/api';
 
 export async function GET (req: NextRequest) {
-    const forwardedFor = req.headers.get('x-forwarded-for');
-    const realIp = req.headers.get('x-real-ip');
+    /**
+     * ? transform headers into a plain object,
+     * ? because request-ip exepecets it rather method-callable next.js headers object
+     */
+    const headers: Record<string, string> = {};
 
-    let ip = null;
+    req.headers.forEach((value, key) => {
+        headers[ key ] = value;
+    });
 
-    if (forwardedFor) {
-        ip = forwardedFor?.split(',')[ 0 ];
-    } else if (realIp) {
-        ip = realIp;
+    const ip = requestIp.getClientIp({ ...req, headers });
+
+    if (process.env.NODE_ENV === 'production') {
+        await Promise.all([ kv.incr(VISITS_ALL_KEY), kv.sadd(UNIQUE_IP_SET_KEY, ip) ]);
     }
-
-    await Promise.all([ kv.incr(VISITS_ALL_KEY), kv.sadd(UNIQUE_IP_KEY, ip) ]);
 
     const [ visitsAll, visitsUnique ] = await Promise.all([
         kv.get<number>(VISITS_ALL_KEY),
-        kv.scard(UNIQUE_IP_KEY),
+        kv.scard(UNIQUE_IP_SET_KEY),
     ]);
 
     return NextResponse.json<GetVisitsResponse>({ ip, visitsAll, visitsUnique });
 }
 
-const VISITS_ALL_KEY = 'visits:all';
-const UNIQUE_IP_KEY = 'unique-ip-set';
+const VISITS_ALL_KEY = 'visits-all';
+const UNIQUE_IP_SET_KEY = 'unique-ip-set';
