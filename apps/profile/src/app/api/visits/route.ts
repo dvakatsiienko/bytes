93d7d579e04bd3bc603cb1020c88c1c8a714a957
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-29 15:33:42 +0300

    🔨 chore(package.json): update dependencies for hackernews, profile and space-explorer-ui apps

--- apps/profile/src/app/api/visits/route.ts
+++ apps/profile/src/app/api/visits/route.ts
@@ -17,6 +17,10 @@ export async function GET (req: NextRequest) {
         headers[ key ] = value;
     });
 
+    console.log(req.headers);
+    // console.log(req.headers instanceof Map, // False!
+    // );
+
     const ip = requestIp.getClientIp({ ...req, headers });
 
     if (process.env.NODE_ENV === 'production') {
