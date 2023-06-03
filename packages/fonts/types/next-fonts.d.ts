a96c78c97bcb810ed98ec2bb8466412693babcbd
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-06-04 01:14:57 +0300

    🌿 fix(hackernews): resolve vercel build fail

--- packages/fonts/types/next-fonts.d.ts
+++ packages/fonts/types/next-fonts.d.ts
@@ -4,15 +4,3 @@ export declare const nextFonts: {
     robotoFlexVRFont: import('next/dist/compiled/@next/font').NextFont,
     manropeVRFont:    import('next/dist/compiled/@next/font').NextFont,
 };
-
-declare module 'fonts' {
-    declare module 'fonts/manrope-vrr-font-face' {}
-    declare module 'fonts/manrope-vrr-font' {}
-    declare module 'fonts/roboto-flex-vrr-font-face' {}
-    declare module 'fonts/roboto-flex-vrr-font' {}
-}
-
-declare module 'fonts/manrope-vrr-font-face' {}
-declare module 'fonts/manrope-vrr-font' {}
-declare module 'fonts/roboto-flex-vrr-font-face' {}
-declare module 'fonts/roboto-flex-vrr-font' {}
