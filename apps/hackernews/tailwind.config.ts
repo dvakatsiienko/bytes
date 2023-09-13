06b08288ab1b80a26926d7f0d5ad8f215630d097
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-09-13 14:42:21 +0300

    🌟 feat(hackernews): connect tailwing

--- apps/hackernews/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-/* Core */
-import type { Config } from 'tailwindcss';
-
-export default {
-    content: [ './src/**/*.{js,ts,jsx,tsx}' ],
-    theme:   { extend: {}},
-    plugins: [],
-} satisfies Config;
