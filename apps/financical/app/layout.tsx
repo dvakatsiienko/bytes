cc08caedc29310238039a3d154a7ec37a6ee9644
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-08 21:42:24 +0200

    financical: update layout source code

--- apps/financical/app/layout.tsx
+++ apps/financical/app/layout.tsx
@@ -1,7 +1,7 @@
-export default function RootLayout ({ children }: { children: React.ReactNode }) {
+export default (props: { children: React.ReactNode }) => {
     return (
         <html lang = 'en'>
-            <body>{children}</body>
+            <body>{props.children}</body>
         </html>
     );
-}
+};
