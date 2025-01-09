78ae950fa7f2c0b7c83af476a25845b22cbeea87
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-10 00:47:32 +0200

    financical: colocate dashboard-related components

--- apps/financical/src/app/dashboard/page.tsx
+++ apps/financical/src/app/dashboard/page.tsx
@@ -1,34 +1,35 @@
-/* Core */
-import { Card } from '@/ui/dashboard/Cards';
-import { RevenueChart } from '@/ui/dashboard/RevenueChart';
-import { LatestInvoices } from '@/ui/dashboard/LatestInvocies';
-import { lusitana } from '@/ui/fonts';
+/* Components */
+import { Card } from './ui/Card';
+import { RevenueChart } from './ui/RevenueChart';
+import { LatestInvoices } from './ui/LatestInvocies';
 
 /* Instruments */
-import { fetchRevenue, fetchLatestInvoices } from '@/lib/data';
+import { fetchRevenueList, fetchLatestInvoicesList, fetchCardData } from '@/lib/data';
+import { lusitana } from '@/ui/fonts';
 
 const Dashboard = async () => {
-    const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
+    const revenueListPromise = fetchRevenueList();
+    const latestInvoicesListPromise = fetchLatestInvoicesList();
+    const cardDataPromise = fetchCardData();
 
-    console.log('🚀 ~ Dashboard ~ revenue:', latestInvoices);
+    const [ revenueList, latestInvoicesList, cardData ] = await Promise.all([
+        revenueListPromise,
+        latestInvoicesListPromise,
+        cardDataPromise,
+    ]);
 
     return (
         <main>
             <h1 className = { `${ lusitana.className } mb-4 text-xl md:text-2xl` }>Dashboard</h1>
             <div className = 'grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
-                {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-                {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-                {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-                {/* <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
+                <Card title = 'Collected' type = 'collected' value = { cardData.totalPaidInvoices } />
+                <Card title = 'Pending' type = 'pending' value = { cardData.totalPendingInvoices } />
+                <Card title = 'Total Invoices' type = 'invoices' value = { cardData.numberOfInvoices } />
+                <Card title = 'Total Customers' type = 'customers' value = { cardData.numberOfCustomers } />
             </div>
             <div className = 'mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
-                <RevenueChart revenue = { revenue } />
-                <LatestInvoices latestInvoices = { latestInvoices } />
+                <RevenueChart revenueList = { revenueList } />
+                <LatestInvoices latestInvoicesList = { latestInvoicesList } />
             </div>
         </main>
     );
--- apps/financical/src/app/dashboard/ui/Card.tsx
+++ apps/financical/src/app/dashboard/ui/Card.tsx
@@ -1,16 +1,10 @@
-/* eslint-disable */
-
+/* Core */
 import { BanknotesIcon, ClockIcon, UserGroupIcon, InboxIcon } from '@heroicons/react/24/outline';
+
+/* Instruments */
 import { lusitana } from '@/ui/fonts';
 
-const iconMap = {
-    collected: BanknotesIcon,
-    customers: UserGroupIcon,
-    pending:   ClockIcon,
-    invoices:  InboxIcon,
-};
-
-export default async function CardWrapper () {
+export const CardWrapper = () => {
     return (
         <>
             {/* NOTE: Uncomment this code in Chapter 9 */}
@@ -25,29 +19,36 @@ export default async function CardWrapper () {
       /> */}
         </>
     );
-}
+};
 
-export const Card = ({
-    title,
-    value,
-    type,
-}: {
-    title: string,
-    value: number | string,
-    type:  'invoices' | 'customers' | 'pending' | 'collected',
-}) => {
-    const Icon = iconMap[ type ];
+export const Card = (props: CardProps) => {
+    const Icon = iconMap[ props.type ];
 
     return (
         <div className = 'rounded-xl bg-gray-50 p-2 shadow-sm'>
             <div className = 'flex p-4'>
                 {Icon ? <Icon className = 'h-5 w-5 text-gray-700' /> : null}
-                <h3 className = 'ml-2 text-sm font-medium'>{title}</h3>
+                <h3 className = 'ml-2 text-sm font-medium'>{props.title}</h3>
             </div>
             <p
                 className = { `${ lusitana.className } truncate rounded-xl bg-white px-4 py-8 text-center text-2xl` }>
-                {value}
+                {props.value}
             </p>
         </div>
     );
 };
+
+/* Helpers */
+const iconMap = {
+    collected: BanknotesIcon,
+    customers: UserGroupIcon,
+    pending:   ClockIcon,
+    invoices:  InboxIcon,
+};
+
+/* Types */
+interface CardProps {
+    title: string,
+    value: number | string,
+    type:  'invoices' | 'customers' | 'pending' | 'collected',
+}
