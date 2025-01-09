3b47f20117c723855af4918ad94644c6ebfbd1f4
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-09 23:14:41 +0200

    financical: fix build error

--- apps/financical/src/app/page.tsx
+++ apps/financical/src/app/page.tsx
@@ -9,7 +9,7 @@ import { AcmeLogo } from '@/app/ui/AcmeLogo';
 /* Instruments */
 import { lusitana } from '@/app/ui/fonts';
 
-export const RootPage = () => {
+const RootPage = () => {
     return (
         <main className = 'flex min-h-screen flex-col p-6'>
             <div className = 'flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52'>
