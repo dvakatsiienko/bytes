56bb5e2ad1000301f285ac5df89d20f2d9c13e11
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-09 22:49:25 +0200

    financical: connect dashboard navigation

---
+++ apps/financical/src/app/dashboard/layout.tsx
@@ -0,0 +1,20 @@
+/* Components */
+import { SideNav } from './ui/Sidenav';
+
+const DashboardLayout = (props: DashboardLayoutProps) => {
+    return (
+        <div className = 'flex h-screen flex-col md:flex-row md:overflow-hidden'>
+            <div className = 'w-full flex-none md:w-64'>
+                <SideNav />
+            </div>
+            <div className = 'flex-grow p-6 md:overflow-y-auto md:p-12'>{props.children}</div>
+        </div>
+    );
+};
+
+/* Types */
+interface DashboardLayoutProps {
+    children: React.ReactNode,
+}
+
+export default DashboardLayout;
