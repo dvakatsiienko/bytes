80cf3f47c92fa09812c961464e3a87450d339e65
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-10 03:28:33 +0200

    financical: rename /dashboard/ui/_skeletons.tsx

--- apps/financical/src/app/dashboard/(overview)/_loading.tsx
+++ apps/financical/src/app/dashboard/(overview)/_loading.tsx
@@ -1,5 +1,5 @@
 /* Components */
-import { DashboardSkeleton } from '@/ui/skeletons';
+import { DashboardSkeleton } from '@/app/dashboard/ui/_Skeletons';
 
 export default () => {
     return <DashboardSkeleton />;
