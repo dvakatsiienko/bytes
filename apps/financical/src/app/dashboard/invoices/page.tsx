92675a3fd3549456cfca4c7ef6ee63b523da6070
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-10 22:35:04 +0200

    financical: connect invoice filtering

--- apps/financical/src/app/dashboard/invoices/page.tsx
+++ apps/financical/src/app/dashboard/invoices/page.tsx
@@ -1,35 +1,47 @@
 /* Core */
 import { Suspense } from 'react';
-import waait from 'waait';
 
 /* Components */
 import { Search } from '@/ui/Search';
-import { InvoicesTable } from '@/ui/invoices/InvoicesTable';
+import { InvoicesTable } from '@/app/dashboard/invoices/ui/InvoicesTable';
 import { CreateInvoice } from '@/ui/invoices/Buttons';
 import { lusitana } from '@/ui/fonts';
 import { InvoicesTableSkeleton } from '@/ui/Skeletons';
 import { Pagination } from './ui/Pagination';
 
-const InvoicesPage = async () => {
-    // await waait(2000);
+const InvoicesPage = async (props: InvoicesPageProps) => {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
 
     return (
         <div className = 'w-full'>
             <div className = 'flex w-full items-center justify-between'>
                 <h1 className = { `${ lusitana.className } text-2xl` }>Invoices</h1>
             </div>
+
             <div className = 'mt-4 flex items-center justify-between gap-2 md:mt-8'>
                 <Search placeholder = 'Search invoices...' />
                 <CreateInvoice />
             </div>
-            {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-      <Table query={query} currentPage={currentPage} />
-    </Suspense> */}
+
+            <Suspense key = { query + currentPage } fallback = { <InvoicesTableSkeleton /> }>
+                <InvoicesTable currentPage = { currentPage } query = { query } />
+            </Suspense>
+
             <div className = 'mt-5 flex w-full justify-center'>
                 {/* <Pagination totalPages={totalPages} /> */}
             </div>
         </div>
     );
 };
+
+/* Types */
+interface InvoicesPageProps {
+    searchParams?: Promise<{
+        query?: string,
+        page?:  string,
+    }>,
+}
 
 export default InvoicesPage;
