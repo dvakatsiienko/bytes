943dd259c72ac3e13eccaca40574dcc5218a3759
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-10 22:35:04 +0200

    financical: connect invoice filtering

---
+++ apps/financical/src/app/dashboard/invoices/ui/InvoiceStatus.tsx
@@ -0,0 +1,30 @@
+/* Core */
+import clsx from 'clsx';
+import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
+
+export const InvoiceStatus = (props: { status: string }) => {
+    const pendingInvoiceJSX = props.status === 'pending' && (
+        <>
+            Pending
+            <ClockIcon className = 'ml-1 w-4 text-gray-500' />
+        </>
+    );
+
+    const paidInvoiceJSX = props.status === 'paid' && (
+        <>
+            Paid
+            <CheckIcon className = 'ml-1 w-4 text-white' />
+        </>
+    );
+
+    return (
+        <span
+            className = { clsx('inline-flex items-center rounded-full px-2 py-1 text-xs', {
+                'bg-gray-100 text-gray-500': props.status === 'pending',
+                'bg-green-500 text-white':   props.status === 'paid',
+            }) }>
+            {pendingInvoiceJSX}
+            {paidInvoiceJSX}
+        </span>
+    );
+};
