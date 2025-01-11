530cf648cc2c0829dad4cbcd22cc7d867f499041
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-12 00:50:05 +0200

    financical: implement very basic validation

--- apps/financical/src/app/dashboard/invoices/create/ui/CreateInvoiceForm.tsx
+++ apps/financical/src/app/dashboard/invoices/create/ui/CreateInvoiceForm.tsx
@@ -19,7 +19,7 @@ import { createInvoice, type CustomerField, type State } from '@/lib';
 export const CreateInvoiceForm = (props: CreateInvoiceFormProps) => {
     const initialState: State = { message: null, errors: {}};
 
-    // const [ state, createInvoiceAction ] = useActionState(createInvoice, initialState);
+    const [ actionState, createInvoiceAction ] = useActionState(createInvoice, initialState);
 
     const customerListJSX = props.customerList.map((customer) => (
         <option key = { customer.id } value = { customer.id }>
@@ -28,7 +28,7 @@ export const CreateInvoiceForm = (props: CreateInvoiceFormProps) => {
     ));
 
     return (
-        <form action = { createInvoice }>
+        <form action = { createInvoiceAction }>
             <div className = 'rounded-md bg-gray-50 p-4 md:p-6'>
                 {/* Customer Name */}
                 <div className = 'mb-4'>
@@ -38,6 +38,7 @@ export const CreateInvoiceForm = (props: CreateInvoiceFormProps) => {
 
                     <div className = 'relative'>
                         <select
+                            aria-describedby = 'customer-error'
                             className = 'peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
                             defaultValue = ''
                             id = 'customer'
@@ -48,6 +49,14 @@ export const CreateInvoiceForm = (props: CreateInvoiceFormProps) => {
                             {customerListJSX}
                         </select>
                         <UserCircleIcon className = 'pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500' />
+
+                        <div aria-atomic = 'true' aria-live = 'polite' id = 'customer-error'>
+                            {actionState.errors?.customerId?.map((error: string) => (
+                                <p key = { error } className = 'mt-2 text-sm text-red-500'>
+                                    {error}
+                                </p>
+                            ))}
+                        </div>
                     </div>
                 </div>
 
