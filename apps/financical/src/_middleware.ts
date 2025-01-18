8c364f062d8b40cf9722caf71aab2ca82cef5ef0
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-18 21:31:02 +0200

    financical: leave a comment with authe creds in _middleware.ts

--- apps/financical/src/_middleware.ts
+++ apps/financical/src/_middleware.ts
@@ -4,6 +4,10 @@ import NextAuth from 'next-auth';
 /* Instruments */
 import { authConfig } from './auth.config';
 
+// ? User creds
+// user@nextmail.com
+// 123456
+
 export const config = {
     // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
     matcher: [ '/((?!api|_next/static|_next/image|.*\\.png$).*)' ],
