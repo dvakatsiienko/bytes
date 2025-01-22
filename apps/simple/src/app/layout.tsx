392617eda5c9ea4a43dd6f90aa07441ad8338207
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-22 19:54:19 +0200

    simple: connect tw

--- apps/simple/src/app/layout.tsx
+++ apps/simple/src/app/layout.tsx
@@ -1,6 +1,12 @@
+/* Instruments */
+import '../theme/globals.css';
+
 export default (props: React.PropsWithChildren) => {
     return (
         <html lang = 'en'>
+            <head>
+                <title>Simple</title>
+            </head>
             <body>{props.children}</body>
         </html>
     );
--- apps/simple/src/app/page.tsx
+++ apps/simple/src/app/page.tsx
@@ -5,6 +5,8 @@ import { sum } from 'utils';
 export default () => {
     return (
         <section>
+            <h1 className = 'text-3xl font-bold underline'>Hello world!</h1>
+
             <h1>
                 Web
                 <br />
