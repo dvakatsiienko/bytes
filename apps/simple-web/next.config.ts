3ad3be0061878be2e5fbca956fee0982fa93ac8d
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-22 19:30:24 +0200

    apps: remove old apps

--- apps/simple-web/next.config.ts
+++
@@ -1,9 +0,0 @@
-/* Core */
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-    reactStrictMode:   true,
-    transpilePackages: [ 'ui', 'utils' ],
-};
-
-export default nextConfig;
