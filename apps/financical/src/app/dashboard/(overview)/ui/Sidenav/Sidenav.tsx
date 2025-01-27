96629a9d6ba5529b6b412aed1d1f5bdc3730c0e7
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-27 23:36:56 +0200

    financical: refactor /customers route

--- apps/financical/src/app/dashboard/(overview)/ui/Sidenav/Sidenav.tsx
+++ apps/financical/src/app/dashboard/(overview)/ui/Sidenav/Sidenav.tsx
@@ -4,8 +4,8 @@ import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
 
 /* Components */
-import { NavLinks } from './Navlinks';
 import { AcmeLogo } from '@/ui/AcmeLogo';
+import { NavLinks } from './Navlinks';
 
 export const Sidenav = () => {
     const signOutAction = async () => {
@@ -25,7 +25,9 @@ export const Sidenav = () => {
 
             <div className = 'flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2'>
                 <NavLinks />
+
                 <div className = 'hidden h-auto w-full grow rounded-md bg-gray-50 md:block' />
+
                 <form action = { signOutAction }>
                     <button className = 'flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'>
                         <PowerIcon className = 'w-6' />
