27cd4903b920b119782e98ad70f94b038866bd59
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-01-29 00:44:36 +0200

    tops: move authentication to server side

--- apps/tops/src/app/Header.tsx
+++ apps/tops/src/app/Header.tsx
@@ -3,28 +3,19 @@
 import { useState } from 'react';
 import { Dialog, DialogPanel } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import cx from 'clsx';
 
-import { signIn, signOut, useSession } from 'next-auth/react';
-
-const navigation = [
-    { name: 'Product', href: '#' },
-    { name: 'Features', href: '#' },
-    { name: 'Marketplace', href: '#' },
-    { name: 'Company', href: '#' },
-];
-
-export const Header = () => {
+export const Header = (props: React.PropsWithChildren) => {
     const [ mobileMenuOpen, setMobileMenuOpen ] = useState(false);
 
-    const { data: session } = useSession();
-
-    // console.log('🚀 ~ Example ~ session:', session);
-
-    const handleSignIn = () => {
-        session ? signOut() : signIn('github');
-    };
-
-    const authText = session ? 'Sign out' : 'Sign in';
+    const linkListJSX = navigation.map((item) => (
+        <a
+            key = { item.name }
+            className = { cx('font-semibold text-gray-900 lg:text-base/6', { '-mx-3 block rounded-lg px-3 py-2 text-base/7': mobileMenuOpen }) }
+            href = { item.href }>
+            {item.name}
+        </a>
+    ));
 
     return (
         <header className = 'absolute inset-x-0 top-0 z-50'>
@@ -39,6 +30,11 @@ export const Header = () => {
                         />
                     </a>
                 </div>
+
+                <div className = 'hidden lg:flex lg:gap-x-12'>{linkListJSX}</div>
+
+                <div className = 'hidden lg:flex lg:flex-1 lg:justify-end'>{props.children}</div>
+
                 <div className = 'flex lg:hidden'>
                     <button
                         className = '-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'
@@ -46,23 +42,6 @@ export const Header = () => {
                         onClick = { () => setMobileMenuOpen(true) }>
                         <span className = 'sr-only'>Open main menu</span>
                         <Bars3Icon aria-hidden = 'true' className = 'size-6' />
-                    </button>
-                </div>
-                <div className = 'hidden lg:flex lg:gap-x-12'>
-                    {navigation.map((item) => (
-                        <a
-                            key = { item.name }
-                            className = 'text-sm/6 font-semibold text-gray-900'
-                            href = { item.href }>
-                            {item.name}
-                        </a>
-                    ))}
-                </div>
-                <div className = 'hidden lg:flex lg:flex-1 lg:justify-end'>
-                    <button
-                        className = 'text-sm/6 font-semibold text-gray-900'
-                        onClick = { handleSignIn }>
-                        {authText} <span aria-hidden = 'true'>&rarr;</span>
                     </button>
                 </div>
             </nav>
@@ -90,23 +69,8 @@ export const Header = () => {
 
                     <div className = 'mt-6 flow-root'>
                         <div className = '-my-6 divide-y divide-gray-500/10'>
-                            <div className = 'space-y-2 py-6'>
-                                {navigation.map((item) => (
-                                    <a
-                                        key = { item.name }
-                                        className = '-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50'
-                                        href = { item.href }>
-                                        {item.name}
-                                    </a>
-                                ))}
-                            </div>
-                            <div className = 'py-6'>
-                                <button
-                                    className = '-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50'
-                                    onClick = { handleSignIn }>
-                                    {authText}
-                                </button>
-                            </div>
+                            <div className = 'space-y-2 py-6'>{linkListJSX}</div>
+                            <div className = 'py-6'>{props.children}</div>
                         </div>
                     </div>
                 </DialogPanel>
@@ -114,3 +78,9 @@ export const Header = () => {
         </header>
     );
 };
+
+/* Helpers */
+const navigation = [
+    { name: 'Games', href: '#' },
+    { name: 'Leaderboards', href: '#' },
+];
