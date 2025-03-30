280700467c44dae89cb2313c4567d23863ee0d5e
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-03-30 18:37:56 +0300

    pristine: format component structure

--- apps/pristine/src/components/Messages.tsx
+++ apps/pristine/src/components/Messages.tsx
@@ -1,34 +1,36 @@
-import type { Message as TMessage } from "ai";
-import { Message } from "./Message";
-import { useScrollToBottom } from "@/lib/hooks/use-scroll-to-bottom";
+/* Core */
+import type { Message as TMessage } from 'ai';
+
+/* Components */
+import { Message } from './Message';
+
+/* Instruments */
+import { useScrollToBottom } from '@/lib/hooks/use-scroll-to-bottom';
 
 export const Messages = ({
-  messages,
-  isLoading,
-  status,
+    messages,
+    isLoading,
+    status,
 }: {
-  messages: TMessage[];
-  isLoading: boolean;
-  status: "error" | "submitted" | "streaming" | "ready";
+    messages: TMessage[];
+    isLoading: boolean;
+    status: 'error' | 'submitted' | 'streaming' | 'ready';
 }) => {
-  const [containerRef, endRef] = useScrollToBottom();
-  return (
-    <div
-      className="flex-1 h-full space-y-4 overflow-y-auto py-8"
-      ref={containerRef}
-    >
-      <div className="max-w-xl mx-auto pt-8">
-        {messages.map((m, i) => (
-          <Message
-            key={i}
-            isLatestMessage={i === messages.length - 1}
-            isLoading={isLoading}
-            message={m}
-            status={status}
-          />
-        ))}
-        <div className="h-1" ref={endRef} />
-      </div>
-    </div>
-  );
+    const [containerRef, endRef] = useScrollToBottom();
+    return (
+        <div className='h-full flex-1 space-y-4 overflow-y-auto py-8' ref={containerRef}>
+            <div className='mx-auto max-w-xl pt-8'>
+                {messages.map((m, i) => (
+                    <Message
+                        key={i}
+                        isLatestMessage={i === messages.length - 1}
+                        isLoading={isLoading}
+                        message={m}
+                        status={status}
+                    />
+                ))}
+                <div className='h-1' ref={endRef} />
+            </div>
+        </div>
+    );
 };
