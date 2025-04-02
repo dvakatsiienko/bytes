95cfd85fe1905879ae5ba5ccd3b6f9e0761aa208
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-04-02 20:48:10 +0300

    chore: clean not neede code

--- apps/pristine/src/app/page.tsx
+++ apps/pristine/src/app/page.tsx
@@ -31,10 +31,9 @@ const Home = () => {
 
                 <Chat />
 
-                {/* TODO fix layout scrolling */}
-                {/* <div
+                <div
                     aria-hidden='true'
-                    className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'>
+                    className='fixed inset-x-0 bottom-0 -z-10 transform-gpu overflow-hidden blur-3xl'>
                     <div
                         className='aspect-1155/678 bg-linear-to-tr relative left-[calc(50%+3rem)] w-[36.125rem] -translate-x-1/2 from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'
                         style={{
@@ -42,7 +41,7 @@ const Home = () => {
                                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
                         }}
                     />
-                </div> */}
+                </div>
             </div>
         </main>
     );
