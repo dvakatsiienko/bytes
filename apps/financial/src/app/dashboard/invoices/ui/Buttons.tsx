bc3d0a0b677156cd558bb61b3cb66711f7481b49
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-04-21 20:34:23 +0300

    📦 packages: bump

--- apps/financial/src/app/dashboard/invoices/ui/Buttons.tsx
+++ apps/financial/src/app/dashboard/invoices/ui/Buttons.tsx
@@ -30,6 +30,7 @@ export const DeleteInvoice = ({ id }: { id: string }) => {
     const deleteInvoiceWithId = deleteInvoice.bind(null, id);
 
     return (
+        // @ts-expect-error - get rid of server actions
         <form action = { deleteInvoiceWithId }>
             <button className = 'rounded-md border border-gray-200 p-2 hover:bg-gray-100'>
                 <span className = 'sr-only'>Delete</span>
