c5d5415810322a04de51523f0a174fd6ddbb5225
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-05-07 20:28:37 +0300

    📦 cv: align svg naming conv

---
+++ apps/cv/src/app/parts/svg/VercelSVG.tsx
@@ -0,0 +1,18 @@
+/* Instruments */
+import type { TSvgProps } from './types';
+
+export const VercelSVG = (props: TSvgProps) => {
+    const { size = 10, ...restProps } = props;
+
+    return (
+        <svg
+            viewBox='0 0 256 222'
+            width={size}
+            height={size}
+            xmlns='http://www.w3.org/2000/svg'
+            preserveAspectRatio='xMidYMid'
+            {...restProps}>
+            <path className='dark:fill-white fill-black' d='m128 0 128 221.705H0z' />
+        </svg>
+    );
+};
--- apps/cv/src/app/parts/toolConfig.tsx
+++ apps/cv/src/app/parts/toolConfig.tsx
@@ -4,177 +4,177 @@ import * as svg from './svg';
 export const toolListCore = [
     {
         name: 'ESNext',
-        icon: svg.ESNext,
+        icon: svg.ESNextSVG,
     },
     {
         name: 'TypeScript',
-        icon: svg.TypeScript,
+        icon: svg.TypeScriptSVG,
     },
     {
         name: 'Node',
-        icon: svg.Node,
+        icon: svg.NodeSVG,
     },
     {
         name: 'React',
-        icon: svg.React,
+        icon: svg.ReactSVG,
     },
     {
         name: 'Next',
-        icon: svg.Next,
+        icon: svg.NextSVG,
     },
 ];
 
 export const toolListAuth = [
     {
         name: 'Clerk',
-        icon: svg.Clerk,
+        icon: svg.ClerkSVG,
     },
     {
         name: 'NextAuth',
-        icon: svg.NextAuth,
+        icon: svg.NextAuthSVG,
     },
 ];
 
 export const toolListState = [
     {
         name: 'Jotai',
-        icon: svg.Jotai,
+        icon: svg.JotaiSVG,
     },
     {
         name: 'Zustand',
-        icon: svg.Zustand,
+        icon: svg.ZustandSVG,
     },
 ];
 
 export const toolListNetwork = [
     {
         name: 'Query',
-        icon: svg.ReactQuery,
+        icon: svg.ReactQuerySVG,
     },
     {
         name: 'GraphQL',
-        icon: svg.GraphQL,
+        icon: svg.GraphQLSVG,
     },
     {
         name: 'Vercel',
-        icon: svg.Vercel,
+        icon: svg.VercelSVG,
     },
 ];
 
 export const toolListDb = [
     {
         name: 'Prisma',
-        icon: svg.Prisma,
+        icon: svg.PrismaSVG,
     },
     {
         name: 'Postgre',
-        icon: svg.PostgreSQL,
+        icon: svg.PostgreSVG,
     },
     {
         name: 'SQLite',
-        icon: svg.SQLite,
+        icon: svg.SQLiteSVG,
     },
     {
         name: 'Convex',
-        icon: svg.Convex,
+        icon: svg.ConvexSVG,
     },
 ];
 
 export const toolListStyles = [
     {
         name: 'Tailwind',
-        icon: svg.Tailwind,
+        icon: svg.TailwindSVG,
     },
     {
         name: 'CSS',
-        icon: svg.CSS,
+        icon: svg.CssSVG,
     },
 ];
 
 export const toolListComponents = [
     {
         name: 'Radix UI',
-        icon: svg.RadixUI,
+        icon: svg.RadixUiSVG,
     },
     {
         name: 'Chadcn UI',
-        icon: svg.ChadCN,
+        icon: svg.ChadcnSVG,
     },
     {
         name: 'Headless UI',
-        icon: svg.HeadlessUI,
+        icon: svg.HeadlessUISVG,
     },
     {
         name: 'Storybook',
-        icon: svg.Storybook,
+        icon: svg.StorybookSVG,
     },
 ];
 
 export const toolListAnimations = [
     {
         name: 'Figma',
-        icon: svg.Figma,
+        icon: svg.FigmaSVG,
     },
     {
         name: 'motion.dev',
-        icon: svg.Motion,
+        icon: svg.MotionSVG,
     },
 ];
 
 export const toolListAi = [
     {
         name: 'Perplexity',
-        icon: svg.PerplexityAI,
+        icon: svg.PerplexityAiSVG,
     },
     {
         name: 'Cursor',
-        icon: svg.Cursor,
+        icon: svg.CursorLightSVG,
     },
     {
         name: 'CodeRabbit',
-        icon: svg.CodeRabbit,
+        icon: svg.CodeRabbitSVG,
     },
 ];
 
 export const toolListBundlers = [
     {
         name: 'Vite',
-        icon: svg.Vite,
+        icon: svg.ViteSVG,
     },
     {
         name: 'swc',
-        icon: svg.Swc,
+        icon: svg.SwcSVG,
     },
     {
         name: 'esbuild',
-        icon: svg.Esbuild,
+        icon: svg.ESBuildSVG,
     },
     {
         name: 'Webpack',
-        icon: svg.Webpack,
+        icon: svg.WebpackSVG,
     },
 ];
 
 export const toolListLLM = [
     {
         name: 'Vercel AI',
-        icon: svg.Vercel,
+        icon: svg.VercelSVG,
     },
     {
         name: 'OpenAI',
-        icon: svg.OpenAI,
+        icon: svg.OpenAiSVG,
     },
     {
         name: 'Anthropic',
-        icon: svg.Anthropic,
+        icon: svg.AnthropicSVG,
     },
     {
         name: 'Groq',
-        icon: svg.GroqAI,
+        icon: svg.GroqAiSVG,
     },
     {
         name: 'OpenRouter',
-        icon: svg.OpenRouter,
+        icon: svg.OpenRouterAaSVG,
     },
 ];
 
@@ -182,11 +182,11 @@ export const toolListLLM = [
 export const toolListCodeQuality = [
     {
         name: 'ESLint',
-        icon: svg.ESLint,
+        icon: svg.ESNextSVG,
     },
     {
         name: 'Prettier',
-        icon: svg.Prettier,
+        icon: svg.PrettierSVG,
     },
 ];
 
