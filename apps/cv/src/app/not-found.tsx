f48df7510db4cb22d17837bee4e2a3b94444254c
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-05-07 21:52:21 +0300

    💭 cv: connect @workspace/ui

--- apps/cv/src/app/not-found.tsx
+++ apps/cv/src/app/not-found.tsx
@@ -1,7 +1,10 @@
+// import { Button } from '@workspace/ui/components/button';
+
 export default function NotFoundPage() {
     return (
         <section className='grid items-center justify-center'>
             <h1>Not Found</h1>
+            {/* <Button variant='destructive'>Click me</Button> */}
         </section>
     );
 }
