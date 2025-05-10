eb6304b3974f71fbafe12045d131c04e4d2a1f19
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-05-11 00:47:57 +0300

    💭 cv: fix theme picker

--- apps/cv/src/components/service/ThemeSwitcher.tsx
+++ apps/cv/src/components/service/ThemeSwitcher.tsx
@@ -3,10 +3,9 @@
 /* Core */
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
+import { Moon, Sun, Monitor } from 'lucide-react';
 
-import { Tabs, TabsTrigger, TabsContent, TabsList } from '@ui/kit/components/tabs';
-import { Bold, Italic, Underline } from 'lucide-react';
-
+/* Components */
 import { ToggleGroup, ToggleGroupItem } from '@ui/kit/components/toggle-group';
 
 /* Instruments */
@@ -14,20 +13,13 @@ import { cn } from '@ui/kit/lib/utils';
 
 export const ThemeSwitcher = (props: ThemeSwitcherProps) => {
     const [mounted, setMounted] = useState(false);
-    const { setTheme, resolvedTheme, theme } = useTheme();
+    const { setTheme, theme } = useTheme();
 
     useEffect(() => {
         setMounted(true);
     }, []);
 
     if (!mounted) return null;
-
-    // const switchTheme = (value: string) => {
-    //     if (value === 'light') setTheme('dark');
-    //     if (value === 'dark') setTheme('light');
-    // };
-    //
-    console.log({ resolvedTheme });
 
     const change = (value: string): void => {
         console.log({ value });
@@ -42,51 +34,51 @@ export const ThemeSwitcher = (props: ThemeSwitcherProps) => {
             onValueChange={change}
             defaultValue='light'
             className={cn(
-                'justify-elf-end z-10 h-fit w-[auto] p-0.5',
+                'h-6 w-[auto] p-[0px]',
                 'bg-background self-center',
                 props.className,
                 //
             )}>
-            {themeList.map((theme) => (
+            {themeList.map((themePreset) => (
                 <ToggleGroupItem
                     className={cn(
-                        'z-20 h-5',
-                        theme.value === theme && 'text-link!',
-                        'cursor-pointer',
-                        'hover:bg-background',
-                        'bg-gray-200',
-                        'dark:data-[state=on]:bg-background-header',
+                        'h-6',
+                        'hover:bg-background dark:bg-background bg-gray-200',
                         'dark:bg-background',
-                        // 'h-6',
+                        'cursor-pointer',
+                        themePreset.value === theme && 'text-link!',
+
+                        // Todo put into theme
+                        'dark:hover:text-link grid h-full w-full items-center text-gray-500 dark:text-gray-400',
+                        'text-center text-xs font-semibold',
+                        // 'dark:data-[state=on]:bg-background-header',
                         // 'data-[state=active]:bg-amber-200!',
                     )}
-                    key={theme.value}
-                    value={theme.value}
-                    aria-label={theme.label}>
-                    {theme.icon}
+                    key={themePreset.value}
+                    value={themePreset.value}
+                    aria-label={themePreset.label}>
+                    {themePreset.icon}
                 </ToggleGroupItem>
             ))}
         </ToggleGroup>
     );
 };
 
-import { Moon, Sun, Monitor } from 'lucide-react';
-
 /* Helpers */
 export const themeList = [
     {
         value: 'light',
-        // label: 'Light',
+        label: 'Light',
         icon: <Sun className='size-[0.9rem] rotate-0 scale-100 text-current transition-all' />,
     },
     {
         value: 'dark',
-        // label: 'Dark',
+        label: 'Dark',
         icon: <Moon className='size-[0.9rem] rotate-45 text-current transition-all' />,
     },
     {
         value: 'system',
-        // label: 'System',
+        label: 'System',
         icon: <Monitor className='size-[0.9rem] text-current transition-all' />,
     },
 ] as const;
