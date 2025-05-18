d5731fae18e1572fd911998a7cdc71b2c2f09045
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-05-18 20:43:27 +0300

    📦 packages: refresh

--- packages/kit/src/components/button.tsx
+++ packages/kit/src/components/button.tsx
@@ -6,21 +6,13 @@ import { cva, type VariantProps } from 'cva';
 /* Instruments */
 import { cn } from '@ui/kit/lib/utils';
 
-function Button({
-    className,
-    variant,
-    size,
-    asChild = false,
-    ...props
-}: React.ComponentProps<'button'> &
-    VariantProps<typeof buttonVariants> & {
-        asChild?: boolean;
-    }) {
+function Button({ className, variant, size, asChild = false, ...props }: ButtonProps) {
     const Comp = asChild ? Slot : 'button';
 
     return <Comp data-slot='button' className={cn(buttonVariants({ variant, size, className }))} {...props} />;
 }
 
+/* Styles */
 const buttonVariants = cva({
     base: "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
     variants: {
@@ -46,5 +38,11 @@ const buttonVariants = cva({
         size: 'default',
     },
 });
+
+/* Types */
+type ButtonProps = React.ComponentProps<'button'> &
+    VariantProps<typeof buttonVariants> & {
+        asChild?: boolean;
+    };
 
 export { Button, buttonVariants };
