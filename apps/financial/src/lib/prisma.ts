0f655ac02cae979037b61769c236769c20353930
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-06-21 12:22:30 +0300

    ⚙️ financial: fix cloudflare db env variable access

--- apps/financial/src/lib/prisma.ts
+++ apps/financial/src/lib/prisma.ts
@@ -2,7 +2,10 @@
 import { PrismaClient } from '~/prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+const prisma = new PrismaClient({
+    datasourceUrl:
+        'prisma+postgres://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiMDFKWThWWFFSNE1DUjhDRUMxUVI1NVBCREMiLCJ0ZW5hbnRfaWQiOiI5ZGUxNzY0Njg3YzViZDdkN2I3MjRmNzg1MjhjOWE2MGEwYWJmN2Q1YjBkMDM3MDE0NDYwN2ZkYmIwMTg3NDRmIiwiaW50ZXJuYWxfc2VjcmV0IjoiZGYwMDMyYTAtM2Q0MS00ZTViLTg1MjItMDBhMzExZjExNjM0In0.es5rR-NJoyZ8YtGQhWkIFKooFYTffeUgEXUYn6i5Q7k',
+}).$extends(withAccelerate());
 
 const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
