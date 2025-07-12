3545c19f2dd2bd33228c1de8933bafa160528517
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-07-12 20:34:01 +0300

    📦 packages: bump

--- apps/x-com-chat/src/components/ui/button.tsx
+++ apps/x-com-chat/src/components/ui/button.tsx
@@ -25,7 +25,7 @@ export const Button = (props: ButtonProps) => {
 /* Styles */
 export const buttonVariants = cva({
     base: cn(
-        'relative inline-flex shrink-0 items-center justify-center bg-amber-100 gap-2 rounded-md',
+        'relative inline-flex shrink-0 items-center justify-center gap-2 rounded-md',
         'text-sm font-medium whitespace-nowrap',
         'outline-none focus-visible:border-ring',
         "focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
