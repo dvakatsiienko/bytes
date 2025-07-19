8615182c4c18e8e800cb6c835cf5da1dd515bb68
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-07-19 13:20:01 +0300

    🏗️ astro: connect theme switch

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -2,6 +2,7 @@
 
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,4 +10,5 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+    integrations: [react()],
 });
