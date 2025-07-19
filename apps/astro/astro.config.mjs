ff9f910891d29c1e85d63fab524716feb61d8dab
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-07-19 13:10:04 +0300

    🏗️ astro: conenct rss feed

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    site: 'https://bytes-astro.vercel.app/',
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
