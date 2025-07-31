84b809d59434bc779d94e7bdad8c980fa44c00e2
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-07-31 23:08:17 +0300

    🐞 surface: delete experimental ppr flag

--- apps/surface/next.config.ts
+++ apps/surface/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   experimental: {
-    ppr: true,
     reactCompiler: true,
   },
   reactStrictMode: true,
