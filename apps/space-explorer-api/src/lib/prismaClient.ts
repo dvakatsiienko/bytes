4e4f2924a3dca2f619ba933fb028463ee89b5776
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-12-22 17:27:47 +0200

    🧹 packages: delete better-sqlite3

--- apps/space-explorer-api/src/lib/prismaClient.ts
+++ apps/space-explorer-api/src/lib/prismaClient.ts
@@ -1,14 +1,11 @@
 import 'dotenv/config';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-// import Database from 'better-sqlite3';
-
 import { PrismaClient } from './prisma-client/client';
 
 const dbPath =
-  // biome-ignore lint/nursery/noUndeclaredEnvVars: <explanation>
+  // biome-ignore lint/nursery/noUndeclaredEnvVars: space-explorer-api does not use turborepo
   process.env.DATABASE_URL?.replace('file:', '') || './prisma/db.sqlite';
-// const db = new Database(dbPath);
 const adapter = new PrismaBetterSqlite3({ url: dbPath });
 
 export const prismaClient = new PrismaClient({ adapter });
